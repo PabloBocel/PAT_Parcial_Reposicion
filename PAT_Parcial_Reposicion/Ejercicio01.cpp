c3c086a1a38e753aa2f1602c6bf939f2fab26fd8
Pablo <andresbocel0@gmail.com>
2023-11-12 23:32:30 -0600

    Findelproyecto:)

--- PAT_Parcial_Reposicion/Ejercicio01.cpp
+++ PAT_Parcial_Reposicion/Ejercicio01.cpp
@@ -2,34 +2,52 @@
 #include "Ejercicio01.h"
 #include "Node.h"
 
+Node<int>* reverseList(Node<int>* head) {
+    Node<int>* prev = nullptr;
+    Node<int>* current = head;
+    Node<int>* next = nullptr;
+    while (current != nullptr) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
 Node<int>* Ejercicio01::addTwoNumbers(Node<int>* l1, int sizeL1, Node<int>* l2, int sizeL2) {
-    Node<int>* result = nullptr;
-    Node<int>* current = nullptr;
+    l1 = reverseList(l1);
+    l2 = reverseList(l2);
 
+    Node<int>* dummyHead = new Node<int>(0);
+    Node<int>* p = l1, * q = l2, * curr = dummyHead;
     int carry = 0;
-
-    while (l1 || l2 || carry) {
-        int num1 = (l1) ? l1->value : 0;
-        int num2 = (l2) ? l2->value : 0;
-
-        int sum = num1 + num2 + carry;
+    while (p != nullptr || q != nullptr) {
+        int x = (p != nullptr) ? p->value : 0;
+        int y = (q != nullptr) ? q->value : 0;
+        int sum = carry + x + y;
         carry = sum / 10;
-
-        if (!result) {
-            result = new Node<int>(sum % 10);
-            current = result;
-        }
-        else {
-            current->next = new Node<int>(sum % 10);
-            current = current->next;
-        }
-
-        if (l1) l1 = l1->next;
-        if (l2) l2 = l2->next;
+        curr->next = new Node<int>(sum % 10);
+        curr = curr->next;
+        if (p != nullptr) p = p->next;
+        if (q != nullptr) q = q->next;
+    }
+    if (carry > 0) {
+        curr->next = new Node<int>(carry);
     }
 
+    Node<int>* result = reverseList(dummyHead->next);
     return result;
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
